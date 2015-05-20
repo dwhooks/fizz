1d43ab2d6933d4c68250178e01beb0ca596b9c94
dwhooks <dwhooks@yahoo.com>
2015-05-19 19:53:07 -0500

    edited js and index files

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,13 +1,14 @@
 $(document).ready (function() {
 	for(var i = 1; i <= 100; i++) {
-		if (i % 3 === 0) {
-			console.log("fizz");
+		if (i % 3 === 0 && i % 5 === 0) {
+			$("body").append("fizz buzz");
 		} else if (i % 5 ===0) {
-			console.log("buzz");
-		} else if (i % 3 === 0 && i % 5 === 0) {
-			console.log("fizz buzz");
+			$("body").append("buzz");
+		} else if (i % 3 === 0) {
+			$("body").append("fizz");
 		} else {
-			console.log(i);
+			$("body").append(i);
 		}
+		$("body").append("<br>");
 	}
 });
